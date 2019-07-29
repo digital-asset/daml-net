a298c1efe97df6cc47a6cd00430dd3dde68827b0
Georg Schneider <georg.schneider@gmail.com>
2019-07-29 10:02:41 +0200

    Add quickstart project

--- README.md
+++ README.md
@@ -17,6 +17,10 @@ This project is a community-driven effort and contributions are welcome. For que
 - `nuget`
 - `msbuild`
 
+## Quick start
+
+The quick start project shows how to read and write to the ledger for the standard skeleton model. You can find instructions on how to run it [here](Daml.Ledger.QuickStart/).
+
 ## Building the library
 
 The library targets `.Net Standard 2.0` so it can be used from projects targeting the .Net Framework, .Net Core or Mono.  Note that the local build process has only been tested on MacOS Mojave (v10.14.5).
--- src/Daml.Ledger.Client/Clients.cs
+++ src/Daml.Ledger.Client/Clients.cs
@@ -3,6 +3,7 @@
 
 namespace Daml.Ledger.Client
 {
+    using Daml.Ledger.Client.Admin;
     using Grpc.Core;
 
     public class Clients
@@ -10,11 +11,13 @@ namespace Daml.Ledger.Client
         public IActiveContractsClient       ActiveContractsClient       { get; private set; }
         public ICommandClient               CommandClient               { get; private set; }
         public ICommandCompletionClient     CommandCompletionClient     { get; private set; }
-        public ICommandSubmissionClient     CommandSubmissionClient { get; private set; }
+        public ICommandSubmissionClient     CommandSubmissionClient     { get; private set; }
         public ILedgerConfigurationClient   LedgerConfigurationClient   { get; private set; }
         public ILedgerIdentityClient        LedgerIdentityClient        { get; private set; }
         public IPackageClient               PackageClient               { get; private set; }
         public ITransactionsClient          TransactionsClient          { get; private set; }
+        public AdminClients                 Admin                       { get; private set; }
+        public TestingClients               Testing                     { get; private set; }
 
         public Clients(Channel channel)
         {
@@ -26,6 +29,32 @@ namespace Daml.Ledger.Client
             this.LedgerIdentityClient       = new LedgerIdentityClient(channel);
             this.PackageClient              = new PackageClient(channel);
             this.TransactionsClient         = new TransactionsClient(channel);
+            this.Admin                      = new AdminClients(channel);
+            this.Testing                    = new TestingClients(channel);
+        }
+
+        public class AdminClients
+        {
+            public IPackageManagementClient PackageManagementClient { get; private set; }
+            public IPartyManagementClient PartyManagementClient { get; private set; }
+
+            public AdminClients(Channel channel)
+            {
+                this.PackageManagementClient = new PackageManagementClient(channel);
+                this.PartyManagementClient = new PartyManagementClient(channel);
+            }
+        }
+
+        public class TestingClients
+        {
+            public ITimeClient TimeClient { get; private set; }
+            public IResetClient ResetClient { get; private set; }
+
+            public TestingClients(Channel channel)
+            {
+                this.TimeClient = new TimeClient(channel);
+                this.ResetClient = new ResetClient(channel);
+            }
         }
     }
 }
