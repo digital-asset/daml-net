7acc770742d17bf404da30d9a364dfcbc059d891
AndrewDrummond <andrew.drummond@calastone.com>
2020-01-30 15:27:29 +0000

    Update AssemblyInfo.cs

--- src/Daml.Ledger.Client/AssemblyInfo.cs
+++ src/Daml.Ledger.Client/AssemblyInfo.cs
@@ -2,34 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-// [assembly: AssemblyTitle("Daml.Ledger.Client")]
-// [assembly: AssemblyDescription("")]
-// [assembly: AssemblyConfiguration("")]
-// [assembly: AssemblyCompany("HP Inc.")]
-// [assembly: AssemblyProduct("Daml.Ledger.Client")]
-// [assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
-// [assembly: AssemblyTrademark("")]
-// [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-// [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-// [assembly: Guid("8a07fafb-9a8e-49df-8c4e-be3eb79b9ec2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// [assembly: AssemblyVersion("1.0.0.0")]
-// [assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("Daml.Ledger.Client.Test")]
